034a1f8b3692365c436a61de68d04ee6dccfed10
AD-007 <gyanprakasht98726@gmail.com>
2026-07-14 13:46:57 +0530

    all checked and Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import globals from 'globals' 
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
